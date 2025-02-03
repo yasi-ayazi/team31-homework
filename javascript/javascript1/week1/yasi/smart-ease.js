f9c028cf77f32e4bc874d62c07b68522bd717611
yasi <yasi.ayazi@gmail.com>
2025-02-03 19:46:50 +0100

    Implemented future age calculator

---
+++ javascript/javascript1/week1/yasi/smart-ease.js
@@ -0,0 +1,7 @@
+console.log("Age-ify:");
+//A future age calculator
+const yearOfBirth=1987;
+const yearFuture=2035;
+const age=yearFuture-yearOfBirth;
+console.log("You will be "+age+" years old in "+yearFuture);
+
