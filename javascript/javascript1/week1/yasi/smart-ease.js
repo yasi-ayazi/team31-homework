8bfc1e5d8898ee99db5c8698011d9253d71a9ea5
yasi <yasi.ayazi@gmail.com>
2025-02-03 19:52:18 +0100

    Implemented house price estimator

--- javascript/javascript1/week1/yasi/smart-ease.js
+++ javascript/javascript1/week1/yasi/smart-ease.js
@@ -16,4 +16,31 @@ if (shouldShowResultInDogYears) {
 } else {
     console.log('Your dog will be ' + dogYear + ' human years old in ' + dogYearFuture + '.');
 }
+//third section
+console.log("Housey Pricey:");
+//A house price estimator
+let volumeInMeters = 8*10*10; //volumeInMeters = width × depth × height 
+let gardenSizeInM2 = 100;  //garden size
+const peterHousePrice = 2500000; // Peter's actual house price
+let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 
+if (housePrice < peterHousePrice) {
+    console.log("Peter's house price is too high");
+} else if (housePrice === peterHousePrice) {
+    console.log("Peter's house price is perfect");
+} else {    
+    console.log("Peter's house price is too low");
+}
+
+volumeInMeters = 5*11*8; // volumeInMeters = width × depth × height 
+gardenSizeInM2 = 70;  // garden size
+housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+const juliaHousePrice = 1000000; // Julia's actual house price
+
+if (housePrice < juliaHousePrice) {
+    console.log("Julia's house price is too high");
+} else if (housePrice === juliaHousePrice) {
+    console.log("Julia's house price is perfect");
+} else {    
+    console.log("Julia's house price is too low");
+}
