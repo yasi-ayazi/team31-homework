d05082e1ea31f558ba9536cdde66fbf084a906ff
yasi <yasi.ayazi@gmail.com>
2025-02-03 19:50:42 +0100

    Implemented dog age calculator

--- javascript/javascript1/week1/yasi/smart-ease.js
+++ javascript/javascript1/week1/yasi/smart-ease.js
@@ -4,4 +4,16 @@ const yearOfBirth=1987;
 const yearFuture=2035;
 const age=yearFuture-yearOfBirth;
 console.log("You will be "+age+" years old in "+yearFuture);
+//new section
+console.log("Goodboy-Oldboy:");
+//A dog age calculator
+const dogYearOfBirth=2017;
+const dogYearFuture = 2045;
+const dogYear = dogYearFuture - dogYearOfBirth;
+const shouldShowResultInDogYears = true;
+if (shouldShowResultInDogYears) {
+    console.log('Your dog will be ' + (dogYear * 7) + ' dog years old in ' + dogYearFuture + '.');
+} else {
+    console.log('Your dog will be ' + dogYear + ' human years old in ' + dogYearFuture + '.');
+}
 
